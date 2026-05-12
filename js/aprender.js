544b9db9bd1a3d1de4b7044d9117225df7de4698
Matheus Nunes França <matheus62574156@edu.df.senac.br>
2026-05-12 17:08:01 -0300

    aprendendo e atualizando o js

---
+++ js/aprender.js
@@ -0,0 +1,36 @@
+/*
+
+//banco de dados
+
+const nomeUsuario = "user1@gmail.com"
+const senhaUsuario1 = "123456"
+const nomeUsuario = "user2@gmail.com"
+const senhaUsuario2 = "123456"
+
+//Variáveis de login
+let nomeDigitado = prompt("Digite seu email:")
+let senhaDigitada = prompt("Digite sua senha:")
+
+//Verificação de login 
+if(nomeDigitado === nomeUsuario1 && senhaDigitada1 === senhaUsuario1)
+{
+    alert("Login bem-sucedido!bem-vindo, " + nomeUsuario1 + "!" )
+}else if(nomeDigitado === nomeUsuario2 && senhaDigitada2 === senhaUsuario2)
+{
+     alert("Login bem-sucedido!bem-vindo, " + nomeUsuario2 + "!" )
+}else{
+    alert("Login falhou! Email ou senha incorretos.")
+    document.body.classList.add("acesso negado")
+
+    
+*/
+
+if(idade >= 18)
+{
+    alert("Você têm a idade minima necessaria para entrar neste site! Bem-vindo!")
+}
+else{
+    alert("Voce não têm a idade minima necessaria para entrar neste site!")
+    document.body.classList.add("acesso negado")
+
+}
